7f913edc4f2012474ce569904d81d80556f1d7ae
Daniel VM <djvm1591@gmail.com>
2024-05-14 20:11:22 -0600

    clase 14-5
    clase 14-5

--- SCRIPTS/02-poo2.js
+++ SCRIPTS/02-poo2.js
@@ -1,23 +1,23 @@
 class CuentaBancaria{
     constructor(saldoInicial){
-        this.saldo = saldoInicial;
+        let saldo = saldoInicial;
         this.depositar = function (cantidad){
             saldo += cantidad;
         }
         this.retirar = function(cantidad){
             if (cantidad <= saldo ) {
-                saldo -=Saldo 
+                saldo -= cantidad 
             }else{
                 return 'Fondos insuficientes limpio';
             }
         }
         this.verSaldo = function(){
-            return this.saldo;
+            return saldo;
         }
     }
 };
 const cuenta = new CuentaBancaria(100)
 cuenta.depositar (200000)  
-cuenta.retirar(100)
+cuenta.retirar(10)
 
 document.getElementById('output2').innerHTML = `El saldo de su cuenta es: $ ${cuenta.verSaldo()}`;
