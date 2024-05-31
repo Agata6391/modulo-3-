a28547ce627b65e7d41a07385a4103db2370cefe
Daniel VM <djvm1591@gmail.com>
2024-05-30 20:19:11 -0600

    clase react
    update y aprendiendo a usar React

---
+++ React/primer_app_react/src/Micomponente.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './MiComponente.css'
+function MiComponente(){
+return(
+    <div className="mi-componente">
+        <h1> Mi primer componente no  me quiere funcionar
+            a pesar del que el profe lo explico, y a mi no me salio :C
+        </h1>
+        <p> me lo jalooo sii</p>
+    </div>
+);
+
+}
+
+export default MiComponente;
